0015a20b6fad6191b13d17d8b7b02b91e2bb796d
EternalWind <eternalwind@live.cn>
2012-03-15 18:50:06 +0800

    UI
    Completed.

---
+++ mobile phone client/NFCShopping/src/scut/bgooo/ui/CompareActivity.java
@@ -0,0 +1,15 @@
+package scut.bgooo.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CompareActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.compare);
+	}
+
+}
