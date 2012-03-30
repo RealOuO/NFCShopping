11ef62f875ee49eca594e2bc70d8bb9248f27498
DongJin Xiao <772792715@qq.com>
2012-03-30 21:24:43 +0800

    优惠信息BL

--- WebSite/WebSite/BLL/DiscountBL.cs
+++ WebSite/WebSite/BLL/DiscountBL.cs
@@ -3,11 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using NFCShoppingWebSite.DAL;
+using NFCShoppingWebSite.Access_Data;
 
 namespace NFCShoppingWebSite.BLL
 {
-    public class DiscountBL
+    public class DiscountBL:IDisposable
     {
+        IDiscountRepository mRepository = new DiscountRepository();
+        /*查询某一期所有的优惠信息*/
+        public IEnumerable<Discount> GetDiscountByDiscountID(Int32 discountID)
+        {
+            return mRepository.GetDiscounts().Where(s => s.discountID == discountID);
+        }
+        /*查询所有的优惠信息*/
+        public IEnumerable<Discount> GetDiscounts()
+        {
+            return mRepository.GetDiscounts();
+        }
+
+        private bool mIsDisposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.mIsDisposed)
+            {
+                if (disposing)
+                {
+                    mRepository.Dispose();
+                }
+            }
+            this.mIsDisposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
 
     }
 }
--- WebSite/WebSite/BLL/DiscountItemBL.cs
+++ WebSite/WebSite/BLL/DiscountItemBL.cs
@@ -3,10 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using NFCShoppingWebSite.DAL;
+using NFCShoppingWebSite.Access_Data;
 
 namespace NFCShoppingWebSite.BLL
 {
-    public class DiscountItemBL
+    public class DiscountItemBL:IDisposable
     {
+        IDiscountItemRepository mRepository = new DiscountItemRepository();
+        /*查询某一期所有的优惠信息*/
+        public IEnumerable<DiscountItem> GetDiscountItemByDiscountID(Int32 discountID)
+        {
+            return mRepository.GetDiscountItems().Where(s => s.discountID == discountID);
+        }
+        /*查询某商品所有的优惠信息*/
+        public IEnumerable<DiscountItem> GetDiscountItemByProductID(Int32 productID)
+        {
+            return mRepository.GetDiscountItems().Where(s => s.productID == productID);
+        }
+        /*查询某一期某商品所有的优惠信息*/
+        public IEnumerable<DiscountItem> GetDiscountItemByDiscountIDProductID(Int32 discountID,Int32 productID)
+        {
+            return mRepository.GetDiscountItems().Where(s => (s.productID == productID && s.discountID == discountID));
+        }
+        /*查询所有的优惠信息*/
+        public IEnumerable<DiscountItem> GetDiscountItems()
+        {
+            return mRepository.GetDiscountItems();
+        }
+
+        private bool mIsDisposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.mIsDisposed)
+            {
+                if (disposing)
+                {
+                    mRepository.Dispose();
+                }
+            }
+            this.mIsDisposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
     }
 }
